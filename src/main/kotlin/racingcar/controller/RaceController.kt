0ed8e0b436858e75fce3076ef7bcfaa7dff2f7e0
권장순 <jsoonworld@gmail.com>
2025-04-13 23:33:56 +0900

    refactor(RaceController) : GameSetup과 RaceRunner를 활용해 실행 흐름을 메시지 기반 구조로 개선

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,39 +1,15 @@
 package racingcar.controller
 
-import racingcar.domain.*
-import racingcar.view.InputView
-import racingcar.view.OutputView
+import racingcar.service.GameSetup
+import racingcar.service.RaceRunner
 
 class RaceController(
-    private val inputView: InputView,
-    private val outputView: OutputView,
-    private val carFactory: CarFactory,
-    private val numberGenerator: NumberGenerator
+    private val setup: GameSetup,
+    private val runner: RaceRunner
 ) {
-    fun run(){
-        val cars = readCarNamesOrExit()
-        val tryTimes = readTryCountOrExit().toInt()
-        val moveCondition = MoveCondition(numberGenerator)
-        val manager = RaceManager(cars, tryTimes, moveCondition, outputView)
-        manager.run()
-    }
-
-    private fun readCarNamesOrExit(): Cars{
-        return try {
-            outputView.printInputCarNameMessage()
-            val input = inputView.readCarNames()
-            carFactory.create(input)
-        } catch (e: IllegalArgumentException) {
-            throw e
-        }
-    }
-
-    private fun readTryCountOrExit(): String{
-        return try {
-            outputView.printInputTryTimesMessage()
-            inputView.readTryTimes()
-        } catch (e: IllegalArgumentException) {
-            throw e
-        }
+    fun run() {
+        val cars = setup.prepareCars()
+        val tryCount = setup.prepareTryCount()
+        runner.run(cars, tryCount)
     }
 }
