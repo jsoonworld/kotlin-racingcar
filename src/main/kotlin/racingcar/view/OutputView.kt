b713c9d7cd371e59275399d3610fb4463a079ba2
권장순 <jsoonworld@gmail.com>
2025-04-13 23:02:09 +0900

    feat(OutputView) : 우승자 출력 기능 printWinners 추가

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,11 +1,13 @@
 package racingcar.view
 
+import racingcar.domain.Car
 import racingcar.domain.Cars
 
 class OutputView {
     private val INPUT_CARNAME_MESSAGE: String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private val INPUT_TRY_TIMES_MESSAGE: String = "시도할 횟수는 몇 회인가요?"
     private val ROUND_RESULT_INTRO_MESSAGE: String = "실행 결과"
+    private val FINAL_WINNER_MESSAGE: String = "최종 우승자"
 
     fun printInputCarNameMessage(){
         println(INPUT_CARNAME_MESSAGE)
@@ -26,4 +28,8 @@ class OutputView {
         println()
     }
 
+    fun printWinners(winners: List<Car>) {
+        val winnerNames = winners.joinToString(", ") { it.name() }
+        println("$FINAL_WINNER_MESSAGE : $winnerNames")
+    }
 }
