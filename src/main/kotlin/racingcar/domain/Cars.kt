3dbc3c20793d76e07d54309857d291d4cc70617a
권장순 <jsoonworld@gmail.com>
2025-04-13 23:01:38 +0900

    feat(Cars) : 우승자 목록을 반환하는 findWinners 기능 구현

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -11,4 +11,9 @@ class Cars(
             }
         }
     }
+
+    fun findWinners(): List<Car> {
+        val maxPosition = values.maxOf { it.position() }
+        return values.filter { it.position() == maxPosition }
+    }
 }
