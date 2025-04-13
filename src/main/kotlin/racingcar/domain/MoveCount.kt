5083c891e9a0911b872c0e1af1ff9768b049d7da
권장순 <jsoonworld@gmail.com>
2025-04-13 20:57:25 +0900

    refactor(MoveCount) : 생성자 메서드를 명시적으로 개선

--- src/main/kotlin/racingcar/domain/MoveCount.kt
+++ src/main/kotlin/racingcar/domain/MoveCount.kt
@@ -8,7 +8,7 @@ value class MoveCount private constructor(private val value: Int) {
     fun value(): Int = value
 
     companion object {
-        val ZERO = MoveCount(0)
+        fun init(): MoveCount = MoveCount(0)
 
         fun from(value: Int): MoveCount {
             require(value >= 0) { "이동 횟수는 0 이상이어야 합니다." }
