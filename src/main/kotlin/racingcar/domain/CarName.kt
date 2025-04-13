f4a39fde23fd2768cad303930899e0c360633d92
권장순 <jsoonworld@gmail.com>
2025-04-13 19:56:30 +0900

    refactor(CarName) : 내부 value 접근 제한 및 코드 스타일 개선

--- src/main/kotlin/racingcar/domain/CarName.kt
+++ src/main/kotlin/racingcar/domain/CarName.kt
@@ -1,14 +1,14 @@
 package racingcar.domain
 
 @JvmInline
-value class CarName private constructor(val value: String) {
+value class CarName private constructor(private val value: String) {
 
     fun value(): String = value
 
-    companion object{
-        fun from(value: String): CarName{
-            require(value.isNotBlank()) {"자동차 이름은 비어 있을 수 없습니다."}
-            require(value.length <= 5) {"자동차 이름은 5자를 초과할 수 없습니다."}
+    companion object {
+        fun from(value: String): CarName {
+            require(value.isNotBlank()) { "자동차 이름은 비어 있을 수 없습니다." }
+            require(value.length <= 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
             return CarName(value)
         }
     }
