b7bd9c3c19afe5da93b781115898ce303345dff5
권장순 <jsoonworld@gmail.com>
2025-04-13 19:22:04 +0900

    feat(OutputView) : 시도할 횟수를 입력 받는 메시지 출력 기능 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -2,9 +2,14 @@ package racingcar.view
 
 class OutputView {
     private val INPUT_CARNAME_MESSAGE: String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private val INPUT_TRY_TIMES_MESSAGE: String = "시도할 횟수는 몇 회인가요?"
 
     fun printInputCarNameMessage(){
         println(INPUT_CARNAME_MESSAGE)
+    }
+
+    fun printInputTryTimesMessage() {
+        println(INPUT_TRY_TIMES_MESSAGE)
     }
 
 }
