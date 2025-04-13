14008cd2598d6cd3f218f8d179068869896b462b
권장순 <jsoonworld@gmail.com>
2025-04-13 22:50:21 +0900

    feat(OutputView) : 실행 결과 출력 기능 및 라운드 결과 출력 기능 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,8 +1,11 @@
 package racingcar.view
 
+import racingcar.domain.Cars
+
 class OutputView {
     private val INPUT_CARNAME_MESSAGE: String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private val INPUT_TRY_TIMES_MESSAGE: String = "시도할 횟수는 몇 회인가요?"
+    private val ROUND_RESULT_INTRO_MESSAGE: String = "실행 결과"
 
     fun printInputCarNameMessage(){
         println(INPUT_CARNAME_MESSAGE)
@@ -10,6 +13,17 @@ class OutputView {
 
     fun printInputTryTimesMessage() {
         println(INPUT_TRY_TIMES_MESSAGE)
+    }
+
+    fun printRoundResultIntro() {
+        println(ROUND_RESULT_INTRO_MESSAGE)
+    }
+
+    fun printRoundResult(cars: Cars) {
+        cars.forEach{ car ->
+            println("${car.name()} : ${"-".repeat(car.position())}")
+        }
+        println()
     }
 
 }
