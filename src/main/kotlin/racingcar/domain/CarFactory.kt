26768283c469a3e0ba4995fe67a2760c248b5771
권장순 <jsoonworld@gmail.com>
2025-04-13 23:34:07 +0900

    refactor(CarFactory) : 문자열 기반 생성 메서드 제거 및 이름 리스트 기반 생성으로 책임 분리

--- src/main/kotlin/racingcar/domain/CarFactory.kt
+++ src/main/kotlin/racingcar/domain/CarFactory.kt
@@ -1,8 +1,7 @@
 package racingcar.domain
 
 class CarFactory {
-    fun create(input: String): Cars {
-        val names = CarNameSplitter(input).split()
+    fun createFromNames(names: List<String>): Cars {
         val cars = names.map { Car(CarName.from(it), MoveCount.init()) }
         return Cars(cars)
     }
