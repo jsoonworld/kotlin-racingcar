9be54f91e650218fb2609cbd1a377eb30d72d57b
권장순 <jsoonworld@gmail.com>
2025-04-13 19:56:42 +0900

    refactor(MoveCount) : value 접근 제어 및 ZERO 상수 추가

--- src/main/kotlin/racingcar/domain/MoveCount.kt
+++ src/main/kotlin/racingcar/domain/MoveCount.kt
@@ -1,13 +1,17 @@
 package racingcar.domain
 
 @JvmInline
-value class MoveCount private constructor(val value: Int){
+value class MoveCount private constructor(private val value: Int) {
 
     fun increase(): MoveCount = MoveCount(value + 1)
 
-    companion object{
+    fun value(): Int = value
+
+    companion object {
+        val ZERO = MoveCount(0)
+
         fun from(value: Int): MoveCount {
-            require(value >= 0){ "이동 횟수는 0 이상이어야 합니다."}
+            require(value >= 0) { "이동 횟수는 0 이상이어야 합니다." }
             return MoveCount(value)
         }
     }
